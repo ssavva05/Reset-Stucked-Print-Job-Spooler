8b978763943a1dfeb7ca7bda43becd46af6f43b7
ssavva05 <ssavva05@cs.ucy.ac.cy>
2022-07-13 21:08:58 +0300

    Update resetStuckedPrintSpooler.bat

--- resetStuckedPrintSpooler.bat
+++ resetStuckedPrintSpooler.bat
@@ -1,4 +1,4 @@
 net stop spooler
-del /F /Q "C:\Windows\System32\Spool\Printers\"
+del /F /Q "C:\Windows\System32\Spool\Printers\*"
 net start spooler
 net stop Print^ Spooler && net start Print^ Spooler
